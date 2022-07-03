7b501a127b4ee2436251c2fcf83035b51deaa259
driver88 <compkim@gmail.com>
2022-07-03 19:14:19 +0900

    add header, logo component and google fonts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory} from 'vue-router'
 import Home from './Home'
 import About from './About'
+import Movie from './Movie'
 
 export default createRouter({
   // Hash 모드를 사용
@@ -11,6 +12,10 @@ export default createRouter({
     {
       path:'/',
       component: Home
+    },
+    {
+      path: '/movie',
+      component: Movie
     },
     {
       path:'/about',
